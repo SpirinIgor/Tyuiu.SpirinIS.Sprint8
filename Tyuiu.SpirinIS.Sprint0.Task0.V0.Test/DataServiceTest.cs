6df354d542165acd1333745c9478d5e7807eded4
SpirinIgor <i_g_o_r_spirin@mail.ru>
2023-09-06 09:28:41 +0500

    Add project files.

---
+++ Tyuiu.SpirinIS.Sprint0.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SpirinIS.Sprint0.Task0.V0.Lib;
+
+namespace Tyuiu.SpirinIS.Sprint0.Task0.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCulc()
+        {
+            DataService ds = new DataService();
+
+            var res = ds.Culc(5, 5);
+
+            Assert.AreEqual(10, res);
+        }
+    }
+}
---
+++ Tyuiu.SpirinIS.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SpirinIS.Sprint0.Task0.V0.Lib;
+
+namespace Tyuiu.SpirinIS.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("Введите значение переменной Х:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            var result = ds.Culc(x, y);
+
+            Console.WriteLine("Результат X+Y = " + result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
